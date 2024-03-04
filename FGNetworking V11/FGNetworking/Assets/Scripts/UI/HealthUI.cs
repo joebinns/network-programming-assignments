96add6aa13f1677e872b927e803985346ab7ad97
joebinns <hello@joebinns.com>
2024-03-04 11:22:43 +0100

    Prevent the health UI and name UI rotating with the player

--- FGNetworking V11/FGNetworking/Assets/Scripts/UI/HealthUI.cs
+++ FGNetworking V11/FGNetworking/Assets/Scripts/UI/HealthUI.cs
@@ -1,24 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
-   [SerializeField] Image loadingBarImage;
-    [SerializeField] Health health;
-   /// <summary>
-   /// Start is called on the frame when a script is enabled just before
-   /// any of the Update methods is called the first time.
-   /// </summary>
-   void Start()
+	[SerializeField] Image loadingBarImage;
+	[SerializeField] Health health;
+	[SerializeField, Range(-1f, 0f)] private float _verticalOffset = -0.6f;
+
+	void Start()
    {
-       health.currentHealth.OnValueChanged += UpdateUI;
+	   health.currentHealth.OnValueChanged += UpdateUI;
    }
 
-    private void UpdateUI(int previousValue, int newValue)
-    {
-        loadingBarImage.fillAmount = (float)newValue / 100;
-    }
+	private void UpdateUI(int previousValue, int newValue)
+	{
+		loadingBarImage.fillAmount = (float)newValue / 100;
+	}
+
+	private void Update()
+	{
+		transform.position = transform.parent.position + _verticalOffset * Vector3.up;
+		transform.rotation = Quaternion.identity;
+	}
 }
--- FGNetworking V11/FGNetworking/Assets/Scripts/UI/NameUI.cs
+++ FGNetworking V11/FGNetworking/Assets/Scripts/UI/NameUI.cs
@@ -4,9 +4,16 @@ public class NameUI : MonoBehaviour
 {
 	[SerializeField] private TextMesh _textMesh;
 	[SerializeField] private Name _name;
+	[SerializeField, Range(0f, 1f)] private float _verticalOffset = 0.6f;
 
-	void Start()
+	private void Start()
 	{
 		_textMesh.text = _name.UserName.Value.ToString();
+	}
+
+	private void Update()
+	{
+		transform.position = transform.parent.position + _verticalOffset * Vector3.up;
+		transform.rotation = Quaternion.identity;
 	}
 }
