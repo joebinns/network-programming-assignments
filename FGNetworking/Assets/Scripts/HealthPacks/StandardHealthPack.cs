4f0f8be4a126b21b912078ce32cea74f04caf83a
joebinns <hello@joebinns.com>
2024-03-10 15:10:34 +0100

    Add standard health pack logic

--- FGNetworking/Assets/Scripts/HealthPacks/StandardHealthPack.cs
+++ FGNetworking/Assets/Scripts/HealthPacks/StandardHealthPack.cs
@@ -1,5 +1,18 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class StandardHealthPack : MonoBehaviour
+public class StandardHealthPack : NetworkBehaviour
 {
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!IsServer) return;
+
+		var health = other.GetComponent<Health>();
+		if (!health) return;
+
+		health.GainHealth(25);
+
+		var networkObject = gameObject.GetComponent<NetworkObject>();
+		networkObject.Despawn();
+	}
 }
--- FGNetworking/Assets/Scripts/Player/Health.cs
+++ FGNetworking/Assets/Scripts/Player/Health.cs
@@ -1,23 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class Health : NetworkBehaviour
 {
-    public NetworkVariable<int> currentHealth = new NetworkVariable<int>();
+	public NetworkVariable<int> currentHealth = new NetworkVariable<int>();
+
+	private const int MIN_HEALTH = 0;
+	private const int MAX_HEALTH = 100;
 
 
-    public override void OnNetworkSpawn()
-    {
-        if(!IsServer) return;
-        currentHealth.Value = 100;
-    }
+	public override void OnNetworkSpawn()
+	{
+		if (!IsServer) return;
 
+		currentHealth.Value = MAX_HEALTH;
+	}
 
-    public void TakeDamage(int damage){
-        damage = damage<0? damage:-damage;
-        currentHealth.Value += damage;
-    }
+	public void GainHealth(int health)
+	{
+		currentHealth.Value += Mathf.Abs(health);
+		currentHealth.Value = Mathf.Min(currentHealth.Value, MAX_HEALTH);
+	}
+
+	public void TakeDamage(int damage)
+	{
+		currentHealth.Value -= Mathf.Abs(damage);
+		currentHealth.Value = Mathf.Max(currentHealth.Value, MIN_HEALTH);
+	}
 
 }
