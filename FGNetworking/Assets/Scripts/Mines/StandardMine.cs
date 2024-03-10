534c25d52cadcd5f386c7ffab04708f0a770de0b
joebinns <hello@joebinns.com>
2024-03-10 19:44:06 +0100

    Format standard mine

--- FGNetworking/Assets/Scripts/Mines/StandardMine.cs
+++ FGNetworking/Assets/Scripts/Mines/StandardMine.cs
@@ -1,44 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class StandardMine : NetworkBehaviour
 {
-    
-   [SerializeField] GameObject minePrefab;
+	[SerializeField] GameObject minePrefab;
 
-   void OnTriggerEnter2D(Collider2D other)
-   {
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (IsServer)
+		{
+			Health health = other.GetComponent<Health>();
+			if (!health) return;
+			health.LoseHealth(25);
 
-    
-    if(IsServer){
+			int xPosition = Random.Range(-4, 4);
+			int yPosition = Random.Range(-2, 2);
 
-        Health health = other.GetComponent<Health>();
-        if(!health) return;
-        health.LoseHealth(25);
-        
-        int xPosition = Random.Range(-4, 4);
-        int yPosition = Random.Range(-2, 2);
+			GameObject newMine = Instantiate(minePrefab, new Vector3(xPosition, yPosition, 0), Quaternion.identity);
+			NetworkObject no = newMine.GetComponent<NetworkObject>();
+			no.Spawn();
 
-
-        GameObject newMine = Instantiate(minePrefab, new Vector3(xPosition, yPosition, 0), Quaternion.identity);
-        NetworkObject no = newMine.GetComponent<NetworkObject>();
-        no.Spawn();
-        
-        
-
-        NetworkObject networkObject = gameObject.GetComponent<NetworkObject>();
-        networkObject.Despawn();
-
-
-
-    }
-
-
-   }
-
-
-
-
+			NetworkObject networkObject = gameObject.GetComponent<NetworkObject>();
+			networkObject.Despawn();
+		}
+	}
 }
